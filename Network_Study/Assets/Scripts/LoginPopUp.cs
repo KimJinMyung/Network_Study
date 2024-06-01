936f51259e4348804b366cb55487673b7dd6ada9
김진명 <https://github.com/KimJinMyung>
2024-06-01 14:47:02 +0900

    [Update] LoginPopup UI 생성

---
+++ Network_Study/Assets/Scripts/LoginPopUp.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginPopUp : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private InputField _Input_ID;
+    [SerializeField] private InputField _Input_Password;
+
+    [SerializeField] private Button _Btn_Host;
+    [SerializeField] private Button _Btn_Client;
+
+
+}
