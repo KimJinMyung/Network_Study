428eaa2cc48bad2c42d1aa91d8d563090ed94d7b
김진명 <https://github.com/KimJinMyung>
2024-06-01 15:49:39 +0900

    [Update] LoginPopUp Scripts 생성 완료, Chatting UI 제작중...

--- Network_Study/Assets/Scripts/LoginPopUp.cs
+++ Network_Study/Assets/Scripts/LoginPopUp.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
 public class LoginPopUp : MonoBehaviour
 {
@@ -12,5 +11,82 @@ public class LoginPopUp : MonoBehaviour
     [SerializeField] private Button _Btn_Host;
     [SerializeField] private Button _Btn_Client;
 
+    public static LoginPopUp instance;
 
+    private string _originalNetworkAddress;
+    private bool isReadyToAccess_ID;
+    private bool isReadyToAccess_Password;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        SetDefaultNetworkAddress();
+    }
+
+    private void SetDefaultNetworkAddress()
+    {
+        if (string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress))
+        {
+            NetworkManager.singleton.networkAddress = "localhost";
+        }
+
+        _originalNetworkAddress = NetworkManager.singleton.networkAddress;
+    }
+
+    private void OnEnable()
+    {
+        _Input_ID.onValueChanged.AddListener(OnValueChanged_ID);
+        _Input_Password.onValueChanged.AddListener(OnValueChanged_Password);
+    }
+
+    private void OnDisable()
+    {
+        _Input_ID.onValueChanged.RemoveListener(OnValueChanged_ID);
+        _Input_Password.onValueChanged.RemoveListener(OnValueChanged_Password);
+    }
+
+    //ID와 Password 모두 입력하면 버튼 활성화
+    public void OnValueChanged_ID(string str)
+    {
+        bool isNotInputFieldNull = !string.IsNullOrWhiteSpace(str);
+        isReadyToAccess_ID = isNotInputFieldNull;
+        OnValueChanged_ToggleButton();
+    }
+
+    public void OnValueChanged_Password(string str)
+    {
+        bool isNotInputFieldNull = !string.IsNullOrWhiteSpace(str);
+        isReadyToAccess_Password = isNotInputFieldNull;
+        OnValueChanged_ToggleButton();
+    }
+
+    private void OnValueChanged_ToggleButton()
+    {
+        bool isReadToAccess = isReadyToAccess_ID && isReadyToAccess_Password;
+        _Btn_Host.interactable = isReadToAccess;
+        _Btn_Client.interactable= isReadToAccess;
+    }
+
+    private void Update()
+    {
+        if (string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress))
+        {
+            NetworkManager.singleton.networkAddress = _originalNetworkAddress;
+        }        
+    }
+
+    public void OnClick_StartHost()
+    {
+        NetworkManager.singleton.StartHost();
+        this.gameObject.SetActive(false);
+    }
+    public void OnClick_StartAsClient()
+    {
+        NetworkManager.singleton.StartClient();
+        this.gameObject.SetActive(false);
+    }
 }
